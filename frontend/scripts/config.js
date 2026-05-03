b037d895bce273fb1f2f7c4406632166afc6623b
CoodeCrafters <151801262+CoodeCrafters@users.noreply.github.com>
2026-05-03 23:38:15 +0530

    Update config.js

--- frontend/scripts/config.js
+++ frontend/scripts/config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
-    API_BASE_URL: 'https://delete-wiley-epson-controlled.trycloudflare.com'
+    API_BASE_URL: 'https://cities-meeting-assurance-ebony.trycloudflare.com'
 };
 window.CONFIG = CONFIG;
