66e742bc54e62d0c98554c49db2f257ff1af3275
CoodeCrafters <151801262+CoodeCrafters@users.noreply.github.com>
2026-07-29 15:19:15 +0530

    Update config.js

--- frontend/scripts/config.js
+++ frontend/scripts/config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
-    API_BASE_URL: 'https://cake-reviewer-conditions-useful.trycloudflare.com'
+    API_BASE_URL: 'https://retrieved-notification-shareware-kills.trycloudflare.com'
 };
 window.CONFIG = CONFIG;
